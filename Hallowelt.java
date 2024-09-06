d7004b87ea04dc3a4bbfca29a79953133e593f88
scape10 <160543616+scape10@users.noreply.github.com>
2024-09-06 13:58:17 +0200

    Hallowelt.java

---
+++ Hallowelt.java
@@ -0,0 +1,13 @@
+public class Hallowelt {
+    
+
+    public void ausgabe(){
+        System.out.println("Hallo Daniel");
+    }
+    public static void main(String[]args){
+        Hallowelt hw1;
+
+        hw1 = new Hallowelt();
+        hw1.ausgabe();
+    }
+}
