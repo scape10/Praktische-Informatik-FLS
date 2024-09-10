4590e824f780da3163bde7cace7258fcebdc8b70
scape10 <160543616+scape10@users.noreply.github.com>
2024-09-10 12:14:49 +0200

    Update Steckbrief.java

--- Steckbrief.java
+++ Steckbrief.java
@@ -5,19 +5,19 @@ public class Steckbrief {
     Scanner sc = new Scanner(System.in);
     int alter;
     String name;
-    String wohnort;
     String straße;
     int hausnummer;
+    String wohnort
 
     public void eingabe(){
         System.out.println("Wie alt sind sie? Geben sie ihr alter ein");
     alter = sc.nextInt();
         System.out.println("Wie Heißen sie? Geben sie ihren Namen ein");
     name = sc.next();
-        System.out.println("Wo Wohnen sie? Geben sie ihren wohnort an");
-    wohnort = sc.next();
         System.out.println("Geben sie ihre straße und Hausnummer an (Erst Straße dann Hausnummer)");
     straße = sc.next(); hausnummer = sc.nextInt();
+        System.out.println("Geben sie ihren wohnort an");
+    wohnort = sc.next();
     }
     public void ausgabe(){
         System.out.println(name + " ist " + alter + " alt und wohn in " + wohnort + " in der " + straße + hausnummer);
