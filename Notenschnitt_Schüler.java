81d4eb475a1cf728e7694ecebf108605da99b74b
scape10 <160543616+scape10@users.noreply.github.com>
2024-09-13 11:10:24 +0200

    Create Notenschnitt_Schüler.java

---
+++ Notenschnitt_Schüler.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class Notenschnitt_Schüler {
+    public static void main(String[] args) {
+       
+        int klausur1 = 2;
+        int klausur2 = 3;
+        int test1 = 4;
+        int test2 = 2;
+        int anzahl = 6;
+        double notenschnitt;
+
+        notenschnitt = (klausur1*2.0 + klausur2*2 + test1 + test2)/anzahl;
+        System.out.println("1.Klausur:  " + klausur1);
+        System.out.println("2.Klausur:  " + klausur2);
+        System.out.println("1.Test: " + test1);
+        System.out.println("2.Test: " + test2);
+        System.out.println("_____________________");
+        System.out.println("Der schüler hat den durchschnitt: " + notenschnitt); // ╰‿╯
+
+    }
+    
+}
