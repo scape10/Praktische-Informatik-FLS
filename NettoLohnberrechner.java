28f46e16b71139cbd782a77b7d3262230478cba6
scape10 <160543616+scape10@users.noreply.github.com>
2024-09-17 12:56:55 +0200

    Update NettoLohnberrechner.java

--- NettoLohnberrechner.java
+++ NettoLohnberrechner.java
@@ -22,7 +22,7 @@ public class NettoLohnberrechner {
         System.out.println("Nettolohn: \t\t" + lohn);
         System.out.println("Lohnsteuer: \t\t" + lohnsteuer);
     }
-    public static void main (String[]args){
+    public static void main (String[]args){        //╰‿╯
         NettoLohnberrechner nlb;
         nlb = new NettoLohnberrechner();
 
