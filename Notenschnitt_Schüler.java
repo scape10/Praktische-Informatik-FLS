9c05748b82f72c3416b213501e0e8c6f7c678500
scape <160543616+scape10@users.noreply.github.com>
2024-09-23 11:02:13 +0200

    Update Notenschnitt_Schüler.java

--- Notenschnitt_Schüler.java
+++ Notenschnitt_Schüler.java
@@ -2,13 +2,24 @@ import java.util.*;
 
 public class Notenschnitt_Schüler {
     public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
        
-        int klausur1 = 2;
-        int klausur2 = 3;
-        int test1 = 4;
-        int test2 = 2;
+        int klausur1;
+        int klausur2;
+        int test1;
+        int test2;
         int anzahl = 6;
         double notenschnitt;
+
+        System.out.println("Geben sie die Note der ersten KLausur an");
+        klausur1 = sc.nextInt();
+        System.out.println("Geben sie die Note der zweiten KLausur an");
+        klausur2 = sc.nextInt();
+        System.out.println("Geben sie die Note des ersten Test's an");
+        test1 = sc.nextInt();
+        System.out.println("Geben sie die Note des zweiten Test's an");
+        test2 = sc.nextInt();
 
         notenschnitt = (klausur1*2.0 + klausur2*2 + test1 + test2)/anzahl; //TypeCast
         System.out.println("1.Klausur:  " + klausur1);
